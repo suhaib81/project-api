4b41b8e6f8958d23e4fa7f149a81d50fea27e258
Suhaib <suhaib81@gmail.com>
2017-05-03 14:24:22 +0200

    run the api

--- database/migrations/sqls/20170429191902-teachers-up.sql
+++ database/migrations/sqls/20170429191902-teachers-up.sql
@@ -2,5 +2,5 @@ CREATE TABLE teachers (
   teacher_id SMALLINT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (student_id)
+  PRIMARY KEY (teacher_id)
 );
---
+++ database/migrations/sqls/20170501182348-products-up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products (
+  product_id SMALLINT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (product_id)
+);
